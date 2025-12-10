f09985b31293c079e05593f525cb5c6ce24ba8d5
Mason Haberle <meh9673@log-1.hpc.nyu.edu>
2025-12-10 09:16:26 -0500

    Bash scripts added

---
+++ experiments/bashScripts/QHRun1.bash
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+args=
+for i in "$@"; do 
+  i="${i//\\/\\\\}"
+  args="${args} \"${i//\"/\\\"}\""
+done
+
+if [[ "${args}" == "" ]]; then args="/bin/bash"; fi
+
+if [[ -e /dev/nvidia0 ]]; then nv="--nv"; fi
+
+if [[ -e /scratch/work/public/singularity/greene-ib-slurm-bind.sh ]]; then
+  source /scratch/work/public/singularity/greene-ib-slurm-bind.sh
+fi
+
+singularity exec ${nv} \
+--overlay /scratch/projects/kaptanoglulab/MH/desc.ext3:ro \
+--overlay /scratch/work/public/singularity/intel-oneapi-2022.1.2.sqf:ro \
+--overlay /scratch/work/public/singularity/openmpi-4.1.2-ubuntu-22.04.1.sqf:ro \
+/scratch/work/public/singularity/ubuntu-22.04.1.sif \
+/bin/bash -c "
+unset -f which
+if [[ -e /ext3/apps/openmpi/4.1.2/env.sh ]]; then source /ext3/apps/openmpi/4.1.2/env.sh; fi
+if [[ -e /ext3/env.sh ]]; then source /ext3/env.sh; fi
+eval ${args}
+
+python DESC/experiments/longQHrun.py 27000 0.2 3
+
+"
